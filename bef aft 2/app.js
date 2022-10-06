e76f8df4c81d89ce5c9050c26a790b6f6ceb91e7
faudi <alexnagornyi17@gmail.com>
2022-10-06 20:57:37 +0300

    pure css anim

--- bef aft 2/app.js
+++ bef aft 2/app.js
@@ -1,12 +1,5 @@
-const container = document.querySelector('.container');
-document.querySelector('.slider').addEventListener('input', (e) => {
+const container = document.querySelector('.Beforecontainer');
+document.querySelector('.Beforeslider').addEventListener('input', (e) => {
     container.style.setProperty('--position', `${e.target.value}%`);
 })
-// setTimeout(() => {
-//
-//
-// },1000)
-// for (let i = 0; i < 30; i++) {
-//     document.querySelector('.container').style.setProperty('--position', `${i}%`);
-//     console.log(i);
-// }
+
