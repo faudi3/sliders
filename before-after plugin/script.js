24fd60c65dc8a0d2abe494afdd4b68898226f3f8
faudi <alexnagornyi17@gmail.com>
2022-10-06 16:04:36 +0300

    before after plugin

---
+++ before-after plugin/script.js
@@ -0,0 +1,54 @@
+function getTemplate(state) {
+    return ` <div class="slider__before" style="width: ${state.width}px">
+        <div class="slider__resize" data-type="resize"></div>
+    </div>
+    <div class="slider__after">`
+}
+
+
+class Slider {
+    constructor(selector, state) {
+        this.$slider = document.getElementById(selector);
+        this.state = {
+            ...state,
+            width: state.width || 512
+        }
+        this.#render(this.state);
+        this.#listen();
+    }
+    #render(state) {
+        this.$slider.innerHTML = getTemplate(state)
+    }
+    #update(props) {
+        this.state = {
+            ...this.state,
+            ...props
+        }
+        this.#render(this.state);
+    }
+    #listen () {
+        this.mouseDownHandler = this.mouseDownHandler.bind(this);
+        this.mouseUpHandler = this.mouseUpHandler.bind(this);
+        this.moveHandler = this.moveHandler.bind(this);
+
+        this.$slider.addEventListener('mousedown', this.mouseDownHandler);
+        this.$slider.addEventListener('mouseup', this.mouseUpHandler)
+    }
+    mouseDownHandler(event) {
+        if (event.target.dataset.type === 'resize') {
+            this.$slider.addEventListener('mousemove',this.moveHandler);
+            this.curentClientX = event.clientX;
+        }
+    }
+    mouseUpHandler(event) {
+        this.$slider.removeEventListener('mousemove',this.moveHandler);
+
+    }
+    moveHandler(event) {
+        let newClientX = this.curentClientX - event.clientX;
+        this.#update({width: this.state.width - newClientX})
+        this.curentClientX = event.clientX;
+    }
+}
+
+const slider = new Slider('slider', {})
